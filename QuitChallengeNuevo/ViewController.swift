7960316716abd317ef08ab9c9acbf863b760d6f4
Alina Daniela Fernández <alinadfj@Macs-MacBook-Pro.local>
2017-10-01 16:42:50 -0500

    Initial Commit

---
+++ QuitChallengeNuevo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuitChallengeNuevo
+//
+//  Created by Alina Daniela Fernández on 01/10/17.
+//  Copyright © 2017 Alina Daniela Fernández. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
